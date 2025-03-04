a1b499330cd168a45d64ed3d648f0b0c88a2418b
ahmed <ahmed.hanie@outlook.com>
2025-03-04 19:17:00 +0000

    Morgan Stanely Interview Redemption team

---
+++ MorganStanely/src/main/java/org/example/MorganStanely.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorganStanely {
+    public static void main(String[] args) {
+        SpringApplication.run(MorganStanely.class, args);
+    }
+}
